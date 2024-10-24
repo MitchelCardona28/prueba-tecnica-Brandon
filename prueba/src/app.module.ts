c95a9923fe6b0960492bd342d60ee04270bd18b6
MitchelCardona28 <michelito428@gmail.com>
2024-10-24 13:28:50 -0500

    add: Task service spec and TypeORM config

--- prueba/src/app.module.ts
+++ prueba/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [TasksModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'Tasks',
+      entities: [],
+      synchronize: true,
+    }),
+    TasksModule,
+    AuthModule
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
